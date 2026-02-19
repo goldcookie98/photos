03bee051749f789857ff041270eab034cc8a1870
Jake Harden <jakeharden@Device.local>
2026-02-19 02:54:57 +0000

    small vite fix

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/photos/', // <--- THIS MUST MATCH YOUR REPO NAME
+})
