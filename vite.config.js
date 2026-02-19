71027eae0d8ba6f477d252c5f438a52a11a88635
Jake Harden <jakeharden@Device.local>
2026-02-19 03:10:58 +0000

    second vite fix and add deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/photos/', // <--- THIS MUST MATCH YOUR REPO NAME
+  base: '/photos/',
 })
